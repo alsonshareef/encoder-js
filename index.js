a92f9093b5767fbb7e748556b31fdf2d135f1922
Alson Shareef <alson.shareef@gmail.com>
2019-01-06 17:38:35 +0800

    Update #1

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let encoder = (number) => {
+let encoder = (codedString) => {
     let encoded = []
-    
+    for(let i = 0; i < codedString.length; i++){
+        if (codedString[i] === '0') {
+                let number = '', type = '00', string = `${type} ${number}`
+                
+                encoded.push(string)
+            } else {
+                console.log('not string 0')
+            }
+        }
     return encoded
 }
+
+console.log(encoder('0'))
