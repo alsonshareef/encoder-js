0ddeb4085239a08d27e0c096ebc6b4c50e4c691c
Alson Shareef <alson.shareef@gmail.com>
2019-01-06 19:58:06 +0800

    Starting on consecutive number counter

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-let encoder = (codedString) => {
-    let encoded = []
-    for(let i = 0; i < codedString.length; i++){
-        if (codedString[i] === '0') {
-                let number = '', type = '00', string = `${type} ${number}`
+// let encoder = (codedString) => {
+//     let encoded = []
+//     for(let i = 0; i < codedString.length; i++){
+//         if (codedString[i] === '0') {
+//                 let number = '', type = '00', string = `${type} ${number}`
                 
-                encoded.push(string)
-            } else {
-                console.log('not string 0')
-            }
+//                 encoded.push(string)
+//             } else {
+//                 console.log('not string 0')
+//             }
+//         }
+//     return encoded
+// }
+
+// console.log(encoder('0'))
+
+
+let counter = (string) => {
+    let numberOfZero = 0, NumberOfOnes = 0
+    for(let i = 0; i < string.length; i++){
+        if (string[i] === '0') {
+            numberOfZero += 1
+        } else {
+            NumberOfOnes += 1
         }
-    return encoded
+    }
+    return `0: ${numberOfZero} \n1: ${NumberOfOnes}`
 }
 
-console.log(encoder('0'))
+console.log(counter('010'))
