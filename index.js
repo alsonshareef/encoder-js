24dd3ad32f83d357f4fc71f2765bc65d10f98df2
Alson Shareef <alson.shareef@gmail.com>
2019-01-08 19:38:02 +0800

    Added functionality for displaying amount of consecutive numbers in a string

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-// let encoder = (codedString) => {
-//     let encoded = []
-//     for(let i = 0; i < codedString.length; i++){
-//         if (codedString[i] === '0') {
-//                 let number = '', type = '00', string = `${type} ${number}`
-                
-//                 encoded.push(string)
-//             } else {
-//                 console.log('not string 0')
-//             }
-//         }
-//     return encoded
-// }
+let encoder = (codedString) => {
+    output = []
+    numberAmount = 0
 
-// console.log(encoder('0'))
-
-
-let counter = (string) => {
-    let numberOfZero = 0, NumberOfOnes = 0
-    for(let i = 0; i < string.length; i++){
-        if (string[i] === '0') {
-            numberOfZero += 1
-        } else {
-            NumberOfOnes += 1
-        }
+    for( let i = 0; i < codedString.length; i++ ){
+        if( i !== 0 ){
+            if( codedString[i] !== codedString[i-1] ){
+                output.push(numberAmount)
+                numberAmount = 0
+            }
+        } 
+        numberAmount++
     }
-    return `0: ${numberOfZero} \n1: ${NumberOfOnes}`
+
+    output.push(numberAmount)
+    return output
 }
 
-console.log(counter('010'))
+console.log(encoder('11100010001'))
